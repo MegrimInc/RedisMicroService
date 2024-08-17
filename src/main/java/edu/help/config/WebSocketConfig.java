89d383a4d8a528557791f2cb926eb0b461b15a3d
Lako35 <wesleyyu1223@gmail.com>
2024-08-16 22:58:03 -0400

    Update WebSocketConfig.java
    Fixed Compilation Error

--- src/main/java/edu/help/config/WebSocketConfig.java
+++ src/main/java/edu/help/config/WebSocketConfig.java
@@ -6,28 +6,25 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 import edu.help.websocket.OrderWebSocketHandler;
+import edu.help.websocket.BartenderWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final OrderWebSocketHandler orderWebSocketHandler;
+    private final BartenderWebSocketHandler bartenderWebSocketHandler;
 
-    public WebSocketConfig(OrderWebSocketHandler orderWebSocketHandler) {
+    public WebSocketConfig(OrderWebSocketHandler orderWebSocketHandler, BartenderWebSocketHandler bartenderWebSocketHandler) {
         this.orderWebSocketHandler = orderWebSocketHandler;
+        this.bartenderWebSocketHandler = bartenderWebSocketHandler;
     }
 
-    @SuppressWarnings("null")
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(orderWebSocketHandler, "/ws/orders")
                 .setAllowedOrigins("*");
-    }
-
-    @SuppressWarnings("null")
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(orderWebSocketHandler, "/ws/bartenders")
+        registry.addHandler(bartenderWebSocketHandler, "/ws/bartenders")
                 .setAllowedOrigins("*");
     }
 }
