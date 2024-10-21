804bd94c15cd3924422184602010659d80cdf451
Chide <barzzy.llc@gmail.com>
2024-10-20 23:41:42 -0400

    changed notfications to production

--- src/main/java/edu/help/websocket/OrderWebSocketHandler.java
+++ src/main/java/edu/help/websocket/OrderWebSocketHandler.java
@@ -48,21 +48,21 @@ public class OrderWebSocketHandler extends TextWebSocketHandler {
             throws InvalidKeyException, SSLException, NoSuchAlgorithmException, IOException {
         this.orderService = orderService;
 
-        // this.apnsClient = new ApnsClientBuilder()
-        //         .setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST) // Use `PRODUCTION_APNS_HOST` for development
-        //         .setSigningKey(ApnsSigningKey.loadFromPkcs8File(
-        //                 new File("/app/AuthKey_4TSCNPNRJC.p8"), // Replace with the path to your .p8 file
-        //                 "6TK33N3VRX",
-        //                 "4TSCNPNRJC"))
-        //         .build();
-
         this.apnsClient = new ApnsClientBuilder()
-                .setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST) // Use `DEVELOPMENT_APNS_HOST` for debugging
+                .setApnsServer(ApnsClientBuilder.PRODUCTION_APNS_HOST) // Use `PRODUCTION_APNS_HOST` for development
                 .setSigningKey(ApnsSigningKey.loadFromPkcs8File(
-                        new File("/app/AuthKey_4TSCNPNRJC.p8"), // Replace with the path to your .p8 file     
+                        new File("/app/AuthKey_4TSCNPNRJC.p8"), // Replace with the path to your .p8 file
                         "6TK33N3VRX",
                         "4TSCNPNRJC"))
                 .build();
+
+        // this.apnsClient = new ApnsClientBuilder()
+        //         .setApnsServer(ApnsClientBuilder.DEVELOPMENT_APNS_HOST) // Use `DEVELOPMENT_APNS_HOST` for debugging
+        //         .setSigningKey(ApnsSigningKey.loadFromPkcs8File(
+        //                 new File("/app/AuthKey_4TSCNPNRJC.p8"), // Replace with the path to your .p8 file     
+        //                 "6TK33N3VRX",
+        //                 "4TSCNPNRJC"))
+        //         .build();
 
         // Configure ObjectMapper to ignore unknown fields
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
