231e9463f59bb8400d654ce41657ca3a987cf0a0
Chide <barzzy.llc@gmail.com>
2025-05-21 20:56:16 -0400

    changed barzzy.site to megrim.com

--- src/main/java/edu/help/config/ApiConfig.java
+++ src/main/java/edu/help/config/ApiConfig.java
@@ -4,5 +4,5 @@ public class ApiConfig {
     public static final String ENV = "test"; // Change to "live" when needed
     public static final String BASE_HTTP_PATH = "/redis-" + ENV + "-http";
     public static final String BASE_WS_PATH = "/redis-" + ENV + "-ws";
-    public static final String FULL_HTTP_PATH = "https://www.barzzy.site/postgres-" + ENV + "-http";
+    public static final String FULL_HTTP_PATH = "https://www.megrim.com/postgres-" + ENV + "-http";
 }
