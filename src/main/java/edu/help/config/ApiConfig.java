2786d6e86c4a868e79546e328facefab212e1e73
Chide <barzzy.llc@gmail.com>
2025-06-29 00:09:10 -0400

    added the env change

--- src/main/java/edu/help/config/ApiConfig.java
+++ src/main/java/edu/help/config/ApiConfig.java
@@ -1,7 +1,7 @@
 package edu.help.config;
 
 public class ApiConfig {
-    public static final String ENV = "live"; // Change to "live" when needed
+    public static final String ENV = System.getenv("API_ENV");
     public static final String BASE_HTTP_PATH = "/redis-" + ENV + "-http";
     public static final String BASE_WS_PATH = "/redis-" + ENV + "-ws";
     public static final String FULL_HTTP_PATH = "https://www.megrim.com/postgres-" + ENV + "-http";
